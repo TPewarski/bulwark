88b9fc22cb3258fb9ee1b1ad2b04d46d3f8a8dff
TPewarski <tjp2122@gmail.com>
2023-12-20 19:43:19 -0500

    Working graphql query

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,5 +1,44 @@
-const Portfolio = () => {
-    return <p>Hello World</p>
-}
+const Portfolio = (props) => {
+  const { portfolio } = props;
+  return (
+    <>
+      <p>{portfolio.name}</p>;<p>{portfolio.description}</p>;
+    </>
+  );
+};
 
-export default Portfolio
+export const getStaticProps = async () => {
+  const PORTFOLIO_ID = "123";
+  let portfolio;
+  const query = ``;
+  try {
+    // portfolio = await fetch("localhost:3000/api/graphql/", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ query }),
+    // });
+    await fetch("http://localhost:3000/api/graphql/", {
+      method: "POST",
+      body: JSON.stringify({
+        query: '{ portfolio(id: "123") { name, description } }',
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  } catch (error) {
+    console.log(error);
+  }
+  console.log(portfolio);
+  return {
+    props: {
+      portfolio: { name: "test", description: "test", value: 0, positions: [] },
+    },
+  };
+};
+
+export default Portfolio;
